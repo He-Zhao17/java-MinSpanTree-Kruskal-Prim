9d6f25fc94b7ba0a83af8a0fb897014805446cff
HeZhao_Peko <zh0208@163.com>
2021-05-10 11:24:52 +0800

    # MinHeap swap()

--- src/main/java/priorityQueue/MinHeap.java
+++ src/main/java/priorityQueue/MinHeap.java
@@ -22,10 +22,31 @@ public class MinHeap {
 
 
     public MinHeap () {
-
+        size = 0;
+        heapArr = new MHNode[size];
+        posArr = new int[size];
     }
 
     public void insert (int nodeId, int priority) {
+        size++;
+        MHNode[] old = heapArr;
+        heapArr = new MHNode[size];
+        for (int i = 0; i < size - 1; i++) {
+            heapArr[i] = old[i];
+        }
+        heapArr[size - 1] = new MHNode(nodeId, priority);
+
+        // up
+        int pointer = size - 1;
+        while ((pointer - 1) / 2 >= 0) { // fater Node >= 0
+            if (heapArr[pointer].cost < heapArr[(pointer - 1) / 2].cost) {
+                MHNode temp = heapArr[pointer];
+                heapArr[pointer] = heapArr[(pointer - 1) / 2];
+                heapArr[(pointer - 1) / 2] = temp;
+            }
+        }
+
+
 
     }
 
@@ -37,6 +58,19 @@ public class MinHeap {
 
     }
 
+
+
+
+    // private functions
+    private void swap (int id1, int id2) {
+        try {
+            MHNode temp = heapArr[id1];
+            heapArr[id1] = heapArr[id2];
+            heapArr[id2] = temp;
+        } catch (Exception ex) {
+            ex.getMessage();
+        }
+    }
 
 
     class MHNode {
