a5cdb0f4a852a9cfc22700a5c966e720d9ea1555
HeZhao_Peko <zh0208@163.com>
2021-05-10 11:06:15 +0800

    # MinHeap funtions init

--- src/main/java/priorityQueue/MinHeap.java
+++ src/main/java/priorityQueue/MinHeap.java
@@ -4,6 +4,51 @@ package priorityQueue;
  *  Used in Prim's algorithm. */
 public class MinHeap {
     // FILL IN CODE
+    private MHNode[] heapArr;
+    private int[] posArr;
+    private int size;
+
+    // get & set
+    public MHNode[] getHeapArr() {
+        return heapArr;
+    }
+    public int[] getPosArr() {
+        return posArr;
+    }
+    public int size() {
+        return size;
+    }
+
+
+
+    public MinHeap () {
+
+    }
+
+    public void insert (int nodeId, int priority) {
+
+    }
+
+    public int removeMin() {
+
+    }
+
+    public void reduceKey (int nodeId, int newPriority) {
+
+    }
+
+
+
+    class MHNode {
+        public int id;
+        public int cost;
+
+        public MHNode(int d, int ost) {
+            id = d;
+            ost = cost;
+        }
+    }
+
 
 
 }
